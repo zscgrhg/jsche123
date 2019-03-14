873464036f4809a30442141506d3862c1a52e90c
zscgrhg <zscgrhg@qq.com>
2019-03-14 21:48:13 +0800

    ss

--- src/main/java/com/example/jsche/Mina.java
+++ src/main/java/com/example/jsche/Mina.java
@@ -2,9 +2,12 @@ package com.example.jsche;
 
 import org.apache.sshd.client.SshClient;
 import org.apache.sshd.client.session.ClientSession;
+import org.apache.sshd.client.subsystem.sftp.SftpClient;
+import org.apache.sshd.client.subsystem.sftp.impl.DefaultSftpClient;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.EnumSet;
 import java.util.concurrent.TimeUnit;
 //https://blog.rabahi.net/?page_id=1689#Using_JSCH
 //https://www.ibm.com/developerworks/cn/java/j-lo-sshauthentication/index.html
@@ -24,6 +27,21 @@ public class Mina {
                     session.executeRemoteCommand("uptime",System.err,
                             Charset.defaultCharset());
             System.out.println(response);
+
+            DefaultSftpClient sftp=new DefaultSftpClient(session);
+
+
+            sftp.mkdir("/root/lwk/test/abc");
+
+            SftpClient.Handle h = sftp.open("/root/lwk/test/test.txt", EnumSet.of(SftpClient.OpenMode.Write));
+            byte[] d = "0123456789\n".getBytes();
+            sftp.write(h, 0, d, 0, d.length);
+            sftp.write(h, d.length, d, 0, d.length);
+
+            SftpClient.Attributes attrs = sftp.stat(h);
+            System.out.println(attrs);
+
+            sftp.close(h);
         }
         catch (IOException e) {
             e.printStackTrace();
